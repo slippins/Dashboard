5e50846e8dfb97bc336a0f12e1892fa42c0ab5ed
Kasper <kasper_klinke@hotmail.com>
2020-11-15 14:01:34 +0100

    rettet i chart.js

--- js/chart.js
+++ js/chart.js
@@ -5,14 +5,14 @@ var salesChart = new Chart(ctx, {
     data: {
         labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
         datasets: [{
-            label: '# of Votes',
+            label: 'Sales per hour',
             data: [3, 0, 5, 15, 43, 50, 53, 41, 60, 80, 82, 112],
             backgroundColor: 'rgba(0, 0, 150, 0.5)',
             borderColor: 'rgba(0, 0, 0, 1)',
             borderWidth: 1,
             responsive: true,
             maintainAspectRatio: true,
-
+            aspectRatio: 3,
         }]
     },
     options: {
