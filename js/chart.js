b869121fa4bd7ddc0fbcd8d4aa93209d517fe8d1
Kasper <kasper_klinke@hotmail.com>
2020-11-15 11:40:51 +0100

    Ændret i chart.js

--- js/chart.js
+++ js/chart.js
@@ -1,38 +1,27 @@
 
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
+var ctx = document.getElementById('salesChart');
+var salesChart = new Chart(ctx, {
     type: 'line',
     data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-    label: '# of Votes',
-    data: [12, 19, 3, 5, 2, 3],
-    backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-    ],
-    borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-    ],
-    borderWidth: 1
-}]
-},
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+        datasets: [{
+            label: '# of Votes',
+            data: [3, 0, 5, 15, 43, 50, 53, 41, 60, 80, 82, 112],
+            backgroundColor: 'rgba(0, 0, 150, 0.5)',
+            borderColor: 'rgba(0, 0, 0, 1)',
+            borderWidth: 1,
+            responsive: true,
+            maintainAspectRatio: true,
+
+        }]
+    },
     options: {
-    scales: {
-    yAxes: [{
-    ticks: {
-    beginAtZero: true
-}
-}]
-}
-}
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
 });
